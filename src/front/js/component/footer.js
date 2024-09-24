d3172078de060af364e04244724a8dd4f4dd1e42
Juan M. Pintos <166875657+JuanMPintos@users.noreply.github.com>
2024-09-24 09:34:13 +0000

    footer corregido

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -2,38 +2,37 @@ import React, { Component } from "react";
 
 export const Footer = () => (
 	<footer>
-  <div class="footer-container">
-    <div class="footer-section">
-      <h3>Hoy no cocino</h3>
-      <p>Reservas en los mejores restaurantes de alta calidad.</p>
-    </div>
-    
-    <div class="footer-section">
-      <h4>Enlaces útiles</h4>
-      <ul>
-        <li><a href="#about">Sobre nosotros</a></li>
-        <li><a href="#contact">Contáctanos</a></li>
-        <li><a href="#faq">Preguntas frecuentes</a></li>
-		<li><a href="#unete">Tienes un restaurant?</a></li>
+		<div class="footer-container">
+			<div class="footer-section">
+				<h3>Hoy no cocino</h3>
+				<p>Reservas en los mejores restaurantes de alta calidad.</p>
+			</div>
 
-      </ul>
-    </div>
-    
-    <div class="footer-section">
-      <h4>Contáctanos</h4>
-      <p>Email: contacto@hoynococino.com</p>
-      <p>Teléfono: +123 456 789</p>
-    </div>
-    
-    <div class="footer-section">
-      <h4>Nuestra redes</h4>
-      <a href="https://facebook.com" target="_blank">Facebook</a> |
-      <a href="https://instagram.com" target="_blank">Instagram</a> |
-      <a href="https://twitter.com" target="_blank">Twitter</a>
-    </div>
-  </div>
-  <div class="footer-bottom">
-    <p>&copy; 2024 Hoy no cocino. Todos los derechos reservados.</p>
-  </div>
-</footer>
+			<div class="footer-section">
+				<h4>Enlaces útiles</h4>
+				<ul>
+					<li><a href="#about">Sobre nosotros</a></li>
+					<li><a href="#faq">Preguntas frecuentes</a></li>
+					<li><a href="#unete">Eres un restaurante?</a></li>
+
+				</ul>
+			</div>
+
+			<div class="footer-section">
+				<h4>Contáctanos</h4>
+				<p>Email: contacto@hoynococino.es</p>
+				<p>Teléfono: +123 456 789</p>
+			</div>
+
+			<div class="footer-section">
+				<h4>Nuestras redes</h4>
+				<a href="https://facebook.com" target="_blank">Facebook</a> |
+				<a href="https://instagram.com" target="_blank">Instagram</a> |
+				<a href="https://twitter.com" target="_blank">Twitter</a>
+			</div>
+		</div>
+		<div class="footer-bottom">
+			<p>&copy; 2024 Hoy no cocino. Todos los derechos reservados.</p>
+		</div>
+	</footer>
 );
