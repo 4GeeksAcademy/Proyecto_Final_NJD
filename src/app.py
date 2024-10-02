998f3c6df8ec7507cc8d76d03eeee9ca3dea49db
Daria <149780226+dariaztva10@users.noreply.github.com>
2024-10-02 14:39:26 +0000

    CLOUDINARY

--- src/app.py
+++ src/app.py
@@ -12,6 +12,11 @@ from api.admin import setup_admin
 from api.commands import setup_commands
 from flask_jwt_extended import JWTManager
 from datetime import timedelta
+#Cloudinary:
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
 
 # from models import Person
 
@@ -39,6 +44,14 @@ db.init_app(app)
 # Inicializar JWTManager
 
 jwt = JWTManager(app)
+
+
+# Configuración de Cloudinary
+cloudinary.config(
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET")
+)
 
 # add the admin
 setup_admin(app)
