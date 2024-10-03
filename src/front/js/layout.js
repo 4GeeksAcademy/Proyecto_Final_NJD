f6e6709cc265acc25ec72fc150a905a89a23aa11
Nelvb <169047839+Nelvb@users.noreply.github.com>
2024-10-03 22:11:25 +0000

    modificado autocompletado inputs usuario

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -10,6 +10,7 @@ import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 import { RestaurantSearch } from "./pages/restaurantSearch"; 
 import { RestaurantDetail } from "./pages/restaurantDetail"; 
+import { RegistroRestaurante } from "./pages/registro_restaurante";
 import PrivateView from "./pages/privateView"; 
 
 const Layout = () => {
