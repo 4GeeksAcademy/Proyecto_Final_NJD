c373719b48b08b3daef1b26c8a1595ed02c40e20
Nelson Valero Barcelona <169047839+Nelvb@users.noreply.github.com>
2025-03-14 22:43:47 +0000

    Corrección de instalación de Cython y setuptools en Render

--- render_build.sh
+++ render_build.sh
@@ -3,14 +3,10 @@
 set -o errexit
 
 # Actualizar herramientas antes de instalar cualquier otra cosa
-pip install --upgrade pip
-pip install --upgrade setuptools
-pip install --upgrade wheel
-pip install --upgrade Cython
+pip install --upgrade pip setuptools wheel Cython build
 
 # Instalar dependencias del proyecto
 npm install
 npm run build
 
-pipenv install
-pipenv run upgrade
+pip install -r requirements.txt
