05f231148ea5b98986bb9d1265621a8c6247ffb3
Nelson Valero Barcelona <169047839+Nelvb@users.noreply.github.com>
2025-03-14 22:59:38 +0000

    Cambiar a PyYAML 6.0 para evitar problemas de compilación

--- render_build.sh
+++ render_build.sh
@@ -1,3 +1,4 @@
+cat > render_build.sh << EOF
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
@@ -6,12 +7,13 @@ set -o errexit
 pip install --upgrade pip setuptools wheel
 pip install Cython==0.29.36 build
 
+# Instalar PyYAML de manera separada primero
+pip install pyyaml==6.0 --no-build-isolation
+
 # Instalar dependencias del proyecto
 npm install
 npm run build
 
-# Usar pip para las dependencias de Python
+# Instalar el resto de dependencias
 pip install -r requirements.txt
-
-# Ejecutar migraciones si es necesario
-# Añade aquí cualquier comando para las migraciones de la base de datos
+EOF
