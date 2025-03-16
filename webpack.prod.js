ddb93c55e1f0b7301f342ddd6561a79c5ed8d4de
Nelson Valero Barcelona <169047839+Nelvb@users.noreply.github.com>
2025-03-16 11:55:04 +0000

    Update backend URL and webpack production config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,13 @@
 const { merge } = require('webpack-merge');
+const path = require('path');  // Añade esta línea
 const common = require('./webpack.common.js');
 const Dotenv = require('dotenv-webpack');
+
 module.exports = merge(common, {
     mode: 'production',
     output: {
+        path: path.resolve(__dirname, 'public'),  // Añade esta línea
+        filename: 'bundle.js',  // Añade esta línea
         publicPath: '/'
     },
     plugins: [
