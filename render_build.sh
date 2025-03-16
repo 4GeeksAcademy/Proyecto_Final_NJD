5f18d6f56449791e55bac9291080090ad3cc7fc8
Nelson Valero Barcelona <169047839+Nelvb@users.noreply.github.com>
2025-03-16 00:51:22 +0000

    Preparando deploy en Render

--- render_build.sh
+++ render_build.sh
@@ -2,9 +2,17 @@
 # exit on error
 set -o errexit
 
+# Actualizar herramientas antes de instalar cualquier otra cosa
+pip install --upgrade pip setuptools wheel
+pip install Cython==0.29.36 build
+
+# Instalar PyYAML de manera separada primero
+pip install pyyaml==6.0 --no-build-isolation
+
+# Instalar dependencias del proyecto
 npm install
 npm run build
 
-pipenv install
-
-pipenv run upgrade
+# Instalar el resto de dependencias
+grep -v "pyyaml" requirements.txt > requirements_without_yaml.txt
+pip install -r requirements_without_yaml.txt
---
+++ src/__init__.py
@@ -0,0 +1,1 @@
+# Inicializa el paquete src
