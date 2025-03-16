f0b0f797f0b263e86467e0404df3fbb4457e11a3
Nelson Valero Barcelona <169047839+Nelvb@users.noreply.github.com>
2025-03-16 01:39:57 +0000

    cambios para redeploy

--- src/app.py
+++ src/app.py
@@ -7,24 +7,33 @@ import os
 from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
+
 from flask_jwt_extended import JWTManager
 from datetime import timedelta
-from flask_cors import CORS  
+from flask_cors import CORS  # <-- Importar CORS
+#API Email:
 from flask_mail import Mail, Message
+
+#Cloudinary:
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
 
-from api.utils import APIException, generate_sitemap
-from api.models import db
-from api.routes import api
-from api.admin import setup_admin
-from api.commands import setup_commands
-from api.setup_categorias import cargar_categorias_iniciales
-from api.setup_restaurantes import cargar_restaurantes_iniciales  
+from src.api.utils import APIException, generate_sitemap
+from src.api.models import db
+from src.api.routes import api
+from src.api.admin import setup_admin
+from src.api.commands import setup_commands
+
+# HARDCODE PARA METER LAS CATEGORIAS Y RESTAURANTES EN CADA RESET / SOLO PARA PRUEBAS
+from src.api.setup_categorias import cargar_categorias_iniciales
+from src.api.setup_restaurantes import cargar_restaurantes_iniciales  
+
+# from models import Person
 
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
-static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public/')
+static_file_dir = os.path.join(os.path.dirname(
+    os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
@@ -33,6 +42,7 @@ CORS(app)
 # Instancia de Mail API email
 mail = Mail()
 
+#API email
 # Configuración de Flask-Mail
 app.config.update(dict(
     DEBUG=False,
@@ -40,17 +50,19 @@ app.config.update(dict(
     MAIL_PORT=587,
     MAIL_USE_TLS=True,
     MAIL_USE_SSL=False,
-    MAIL_USERNAME=os.getenv("MAIL_USERNAME"),  
-    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),  
+    MAIL_USERNAME=os.getenv("MAIL_USERNAME"),  # esto en .env
+    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),  # esto en .env
     MAIL_DEFAULT_SENDER = 'hoynococino.ceo@gmail.com'
 ))
 
+# Inicio Mail
 mail.init_app(app)
 
-# Configuración de la base de datos
+# database condiguration
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
+        "postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 
@@ -61,6 +73,7 @@ app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=24)
 MIGRATE = Migrate(app, db, compare_type=True)
 db.init_app(app)
 
+# CARGAR CATEGORIAS Y RESTAURANTES INICIALES SI NO ESTÁN EN LA BASE DE DATOS / SOLO PARA PRUEBAS
 with app.app_context():
     try:
         cargar_categorias_iniciales()
@@ -71,6 +84,7 @@ with app.app_context():
 
 jwt = JWTManager(app)
 
+# Configuración de Cloudinary
 cloudinary.config(
     cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
     api_key=os.getenv("CLOUDINARY_API_KEY"),
@@ -78,7 +92,9 @@ cloudinary.config(
 )
 
 setup_admin(app)
+
 setup_commands(app)
+
 app.register_blueprint(api, url_prefix='/api')
 
 @app.errorhandler(APIException)
@@ -108,10 +124,12 @@ def send_mail():
     reservation_time = data.get('reservation_time')  
 
     try:
+        # Crear el mensaje de correo
         msg = Message(subject="Confirmación de Reserva",
                       sender=os.getenv("MAIL_USERNAME"),
                       recipients=[user_email])
 
+        # Cuerpo del correo en HTML
         msg.html = f"""
             <h3>Confirmación de tu reserva</h3>
             <p>Gracias por reservar en <strong>{restaurant_name}</strong>.</p>
@@ -123,12 +141,17 @@ def send_mail():
             <p>Nos vemos pronto!</p>
         """
 
+        # Enviar el correo
         mail.send(msg)
+
         return jsonify({"message": "Correo enviado exitosamente"}), 200
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+
+
+# this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3001))
     app.run(host='0.0.0.0', port=PORT, debug=True)
